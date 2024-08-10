b1918bb4770b5b845a6807bbd8acdf2ebc51c549
FaintShadow <tybinisafwen590@gmail.com>
2024-08-10 17:02:17 +0100

    Improving positioning system (Rollback canceled)

--- src/game/utilities/Position.java
+++ src/game/utilities/Position.java
@@ -2,10 +2,14 @@ package game.utilities;
 
 import com.raylib.Jaylib;
 import com.raylib.Raylib;
+import game.camera.AdvancedCamera2D;
+import game.utilities.errors.IllegalMethodUsage;
 import game.utilities.errors.IllegalPositioningSystemArgument;
 
+import java.lang.reflect.Method;
 import java.util.Arrays;
 
+import static com.raylib.Raylib.GetScreenToWorld2D;
 import static game.utilities.Variables.*;
 import static game.world.type.BaseWorld.TILESIZE;
 
@@ -87,9 +91,18 @@ public class Position {
     // ----------------------------------------------------------
 
     // Stream conversion:
-    // Screen conversion is still unfinished in all of these methods.
+    // Screen conversion is still unfinished in all of methods below.
     // TODO: Finish conversion to screen on all the methods below.
-    public Position toWorld() {
+
+    /**
+     * Used to make a duplicate of the current position, Use this when you want to convert a position without changing it
+     * @return Duplicate of the same position
+     */
+    public Position duplicate(){
+        return new Position(position.x(), position.y(), system);
+    }
+
+    public Position toWorld() throws NoSuchMethodException, IllegalMethodUsage {
         switch (getSystem()) {
             case W_WORLD:
                 break;
@@ -104,7 +117,7 @@ public class Position {
                 break;
             }
             case W_SCREEN: {
-                return screen();
+                throw new IllegalMethodUsage(Position.class.getMethod("toWorld"), "converting ONLY tile and chunk positions to world positions");
             }
             default:
                 throw new IllegalPositioningSystemArgument(system);
@@ -112,7 +125,25 @@ public class Position {
         return world();
     }
 
-    public Position toTile() {
+    /**
+     * This method is only used to convert screen position to world position, if you're converting another
+     * position type please use the other toWorld method
+     * @param camera
+     * @return A world position
+     * @throws NoSuchMethodException
+     * @throws IllegalMethodUsage
+     */
+    public Position toWorld(AdvancedCamera2D camera) throws NoSuchMethodException, IllegalMethodUsage {
+        if (getSystem().equals(W_SCREEN)) {
+            Raylib.Vector2 pos = GetScreenToWorld2D(getVector2(), camera);
+            x( pos.x() );
+            y( pos.y() );
+            return screen();
+        }
+        throw new IllegalMethodUsage(Position.class.getMethod("toWorld"), system);
+    }
+
+    public Position toTile() throws NoSuchMethodException, IllegalMethodUsage {
         switch (getSystem()) {
             case W_TILE:
                 break;
@@ -127,14 +158,35 @@ public class Position {
                 break;
             }
             case W_SCREEN:
-                return screen();
+                throw new IllegalMethodUsage(Position.class.getMethod("toTile"), "converting ONLY tile and chunk positions to world positions");
             default:
                 throw new IllegalPositioningSystemArgument(system);
         }
         return tile();
     }
 
-    public Position toChunk() {
+    /**
+     * This method is only used to convert screen position to tile position, if you're converting another
+     * position type please use the other toTile method
+     * @param camera
+     * @return A Tile position of the screen position
+     * @throws NoSuchMethodException
+     * @throws IllegalMethodUsage
+     */
+    public Position toTile(AdvancedCamera2D camera) throws NoSuchMethodException, IllegalMethodUsage {
+        if (getSystem().equals(W_SCREEN)) {
+            Raylib.Vector2 pos = GetScreenToWorld2D(getVector2(), camera);
+            x( pos.x() );
+            y( pos.y() );
+            return toTile();
+        }
+        Method method = new Object() {}
+                .getClass()
+                .getEnclosingMethod();
+        throw new IllegalMethodUsage(method, "converting ONLY screen positions to chunk positions");
+    }
+
+    public Position toChunk() throws NoSuchMethodException, IllegalMethodUsage {
         switch (getSystem()) {
             case W_CHUNK:
                 break;
@@ -149,11 +201,29 @@ public class Position {
                 break;
             }
             case W_SCREEN:
-                break;
+                throw new IllegalMethodUsage(Position.class.getMethod("toChunk"), "converting ONLY tile and world positions to chunk positions");
             default:
                 throw new IllegalPositioningSystemArgument(system);
         }
         return chunk();
+    }
+
+    /**
+     * This method is only used to convert screen position to chunk position, if you're converting another
+     * position type please use the other toChunk method
+     * @param camera
+     * @return A chunk position of the screen position
+     * @throws NoSuchMethodException
+     * @throws IllegalMethodUsage
+     */
+    public Position toChunk(AdvancedCamera2D camera) throws NoSuchMethodException, IllegalMethodUsage {
+        if (getSystem().equals(W_SCREEN)) {
+            Raylib.Vector2 pos = GetScreenToWorld2D(getVector2(), camera);
+            x( pos.x() );
+            y( pos.y() );
+            return toChunk();
+        }
+        throw new IllegalMethodUsage(Position.class.getMethod("toChunk"), "converting ONLY screen positions to chunk positions");
     }
     // ----------------------------------------------------------
 }
